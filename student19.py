275f69257351d250c775fdcc9334ff55287a1334
JFDesjardins73 <jean-francois.desjardins2@mrnf.gouv.qc.ca>
2024-01-18 18:44:24 -0500

    Ajout des type annotations et compatible PEP8

--- student19.py
+++ student19.py
@@ -1,1 +1,45 @@
-# Adds @property 
+class Student:
+
+    def __init__(self, name: str, house: str):
+        self.name = name
+        self.house = house
+
+    def __str__(self) -> str:
+        return f"{self.name} from {self.house}"
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        if not name:
+            raise ValueError("Invalid name")
+        self._name = name
+
+    @property
+    def house(self) -> str:
+        return self.__house
+
+    @house.setter
+    def house(self, house: str) -> None:
+        valid_houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin", "Quebec"]
+        if house not in valid_houses:
+            raise ValueError("Invalid house")
+        self.__house = house
+
+
+def get_student() -> Student:
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
+    return student
+
+
+def main() -> None:
+    student = get_student()
+    print(student)
+
+
+if __name__ == "__main__":
+    main()
