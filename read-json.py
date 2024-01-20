846285e1543de26f0c3f22c1edb00a47ea9ac3d7
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-01-19 22:37:54 -0500

    ajout de data.json et read-json.py

---
+++ read-json.py
@@ -0,0 +1,32 @@
+import os
+import json
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+    name: str
+    age: int
+    city: str
+
+# Specify the path to your JSON file
+# json_file_path = 'path/to/your/data.json'
+
+# Get the current directory of the Python script
+script_dir = os.path.dirname(__file__)
+
+# Specify the relative path to your JSON file
+json_file_path = os.path.join(script_dir, 'data.json')
+
+# Open the file and load the JSON data
+with open(json_file_path, 'r', encoding='utf-8') as file:
+    json_data = json.load(file)
+
+# Print the deserialized data
+print(json_data)
+
+# Deserialize the JSON array into a list of Person objects
+people = [Person(**person_data) for person_data in json_data]
+
+# Print the deserialized Person objects
+for person in people:
+    print(f"Name: {person.name}, Age: {person.age}, City: {person.city}")
