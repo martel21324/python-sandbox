d0887823ed3e9d1b1f615e5ba78361920788f4c0
Jean-François Desjardins <jfdesjardins2000@yahoo.ca>
2024-01-21 21:24:50 -0500

    ajout de src/calculator.py

---
+++ src/calculator.py
@@ -0,0 +1,14 @@
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def substract(x: float, y: float) -> float:
+    return x - y
+
+
+def multiply(x: float, y: float) -> float:
+    return x * y
+
+
+def divide(x: float, y: float) -> float:
+    return x / y
